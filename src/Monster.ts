b6e009cbb9e0cbc7e41d566c06e304d3650ff4fb
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-07-10 19:51:35 -0300

    Requisito 9: Concluído

---
+++ src/Monster.ts
@@ -0,0 +1,29 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor(lifePoints = 85, strength = 63) {
+    this._lifePoints = lifePoints;
+    this._strength = strength;
+  }
+
+  get strength() {
+    return this._strength;
+  }
+
+  get lifePoints() {
+    return this._lifePoints;
+  }
+
+  public attack = (enemy: SimpleFighter): void => {
+    enemy.receiveDamage(this.strength);
+  };
+
+  public receiveDamage = (attackPoints: number): number => {
+    this._lifePoints -= attackPoints;
+    this._lifePoints = this.lifePoints < 0 ? -1 : this.lifePoints;
+    return this._lifePoints;
+  };
+}
